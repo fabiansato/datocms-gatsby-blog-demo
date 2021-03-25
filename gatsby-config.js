7a724adb8c691f217d39579b50cd226a20ed5c01
Fabian Gonzalez <fabiangonzalez@ustudio.global>
2021-03-24 21:10:40 -0300

    gatsby
    gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,19 @@
 require('dotenv').config();
 
-const { ACCESS_TOKEN, SPACE_ID, ANALYTICS_ID } = process.env;
-
-const plugins = [
-  {
-    resolve: `gatsby-theme-mate`,
-    options: {
-      accessToken: ACCESS_TOKEN,
-      spaceId: SPACE_ID,
-    },
-  },
-];
-
-if (ANALYTICS_ID) {
-  plugins.push({
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      trackingId: ANALYTICS_ID,
-    },
-  });
-}
-
 module.exports = {
-  plugins,
+  siteMetadata: {
+    title: "Gatsby Blog Demo",
+  },
+  plugins: [
+    'gatsby-plugin-postcss',
+    {
+      resolve: "gatsby-source-datocms",
+      options: {
+        apiToken: process.env.DATO_API_TOKEN,
+        environment: process.env.DATO_ENVIRONMENT,
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+  ],
 };
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from 'gatsby'
+
+export default function Header() {
+    return ( <
+        h2 className = "text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8" >
+        <
+        Link to = "/"
+        className = "hover:underline" >
+        Code Sato <
+        /Link>
+        . <
+        /h2>
+    )
+}
